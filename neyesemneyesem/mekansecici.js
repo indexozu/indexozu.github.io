6f04af080e1a606a3c230099cac4bb294b7b25f0
bthngn <82588968+bthngn@users.noreply.github.com>
2022-08-11 17:56:20 +0300

    Update mekansecici.js

--- neyesemneyesem/mekansecici.js
+++ neyesemneyesem/mekansecici.js
@@ -1,13 +1,41 @@
 const mekan = ["McDonalds'da", "The Bigos'da", "Pizza Il'Forno'da", "evde pişirip"];
-
 const yan = ["ayran", "Coca-Cola Zero", "Niğde Gazozu", "yeşillik"];
 
-const randommekan = Math.floor(Math.random() * mekan.length);
-const randomyan = Math.floor(Math.random() * yan.length);
+var randommekan = Math.floor(Math.random() * mekan.length);
+var randomyan = Math.floor(Math.random() * yan.length);
 
+var body = document.querySelector('body');
+var div = document.createElement('div');
 
-document.write(`
-<h1>Ne yesem ne yesem?</h1>
-<p>Bence bugün <b>${mekan[randommekan]}</b> ye. Yanına da <b>${yan[randomyan]}</b> iyi gider.</p>
-<button onClick="window.location.reload()">aklına yatmadıysa tekrar dene</button>
-<a href="/"><button>megaliste geri dön</button></a>`)
+var h1yap = document.createElement('h1');
+var pyap = document.createElement('p');
+var tekrar = document.createElement('button');
+var geridon = document.createElement('button');
+
+function acilis(){
+    h1yap.appendChild(document.createTextNode('Ne yesem ne yesem?'));
+
+    pyap.appendChild(document.createTextNode(`Bence bugün ${mekan[randommekan]} ye. Yanına da ${yan[randomyan]} iyi gider.`));
+
+    tekrar.setAttribute('onclick', 'yine()');
+    tekrar.appendChild(document.createTextNode('aklına yatmadıysa tekrar dene'));
+
+    geridon.setAttribute('onclick', 'history.back()');
+    geridon.appendChild(document.createTextNode('megaliste geri dön'));
+
+    div.appendChild(h1yap);
+    div.appendChild(pyap);
+    div.appendChild(tekrar);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(geridon);    
+};
+
+body.appendChild(div);
+
+acilis();
+
+function yine(){
+    var randommekan = Math.floor(Math.random() * mekan.length);
+    var randomyan = Math.floor(Math.random() * yan.length);
+    document.querySelector('p').innerHTML = `Bence bugün ${mekan[randommekan]} ye. Yanına da ${yan[randomyan]} iyi gider.`
+};
