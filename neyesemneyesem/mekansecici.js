230c7f6c0dc1b053377628ac548af28c1c8e3e5d
bthngn <82588968+bthngn@users.noreply.github.com>
2022-08-11 19:06:57 +0300

    Update mekansecici.js

--- neyesemneyesem/mekansecici.js
+++ neyesemneyesem/mekansecici.js
@@ -1,41 +1,34 @@
-const mekan = ["McDonalds'da", "The Bigos'da", "Pizza Il'Forno'da", "evde pişirip"];
-const yan = ["ayran", "Coca-Cola Zero", "Niğde Gazozu", "yeşillik"];
+const mekan = ["McDonalds'da", "The Bigos'da", "Pizza Il'Forno'da", "evde pişirip"]
+const yan = ["ayran", "Coca-Cola Zero", "Niğde Gazozu", "yeşillik"]
 
-var randommekan = Math.floor(Math.random() * mekan.length);
-var randomyan = Math.floor(Math.random() * yan.length);
+var randommekan = Math.floor(Math.random() * mekan.length)
+var randomyan = Math.floor(Math.random() * yan.length)
 
-var body = document.querySelector('body');
-var div = document.createElement('div');
+var body = document.querySelector('body')
+var div = document.createElement('div')
 
-var h1yap = document.createElement('h1');
-var pyap = document.createElement('p');
-var tekrar = document.createElement('button');
-var geridon = document.createElement('button');
+var h1yap = document.createElement('h1')
+var pyap = document.createElement('p')
+var tekrar = document.createElement('button')
+var geridon = document.createElement('button')
 
-function acilis(){
-    h1yap.appendChild(document.createTextNode('Ne yesem ne yesem?'));
+body.appendChild(div)
 
-    pyap.appendChild(document.createTextNode(`Bence bugün ${mekan[randommekan]} ye. Yanına da ${yan[randomyan]} iyi gider.`));
+h1yap.appendChild(document.createTextNode('Ne yesem ne yesem?'))
 
-    tekrar.setAttribute('onclick', 'yine()');
-    tekrar.appendChild(document.createTextNode('aklına yatmadıysa tekrar dene'));
+pyap.appendChild(document.createTextNode(`Bence bugün ${mekan[randommekan]} ye. Yanına da ${yan[randomyan]} iyi gider.`))
 
-    geridon.setAttribute('onclick', 'history.back()');
-    geridon.appendChild(document.createTextNode('megaliste geri dön'));
+tekrar.setAttribute('onclick', 'yine()')
+tekrar.appendChild(document.createTextNode('aklına yatmadıysa tekrar dene'))
 
-    div.appendChild(h1yap);
-    div.appendChild(pyap);
-    div.appendChild(tekrar);
-    div.appendChild(document.createElement('br'));
-    div.appendChild(geridon);    
-};
+geridon.setAttribute('onclick', 'history.back()')
+geridon.appendChild(document.createTextNode('megaliste geri dön'))
 
-body.appendChild(div);
-
-acilis();
+var elements = [h1yap, pyap, tekrar, geridon]
+    for (let i = 0; i <= elements.length; i++) {div.appendChild(elements[i])}
 
 function yine(){
-    var randommekan = Math.floor(Math.random() * mekan.length);
-    var randomyan = Math.floor(Math.random() * yan.length);
+    var randommekan = Math.floor(Math.random() * mekan.length)
+    var randomyan = Math.floor(Math.random() * yan.length)
     document.querySelector('p').innerHTML = `Bence bugün ${mekan[randommekan]} ye. Yanına da ${yan[randomyan]} iyi gider.`
-};
+}
